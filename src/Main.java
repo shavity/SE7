518c23c8d623dfb58be061449374fa5cd1ce234d
shavity <y.shavit.mail@gmail.com>
2020-06-09 10:55:48 +0300

    בן זונההההההה

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code her
-        //dkfkgljdflgjfkgjgkfdg
-
-        //gfgdfgdfg
+	//cosomo inaal haolam
     }
 }
